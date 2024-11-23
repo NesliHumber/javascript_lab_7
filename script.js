648c0558412a64462dc2fe7a530c74e048f94fb8
NesliUsta <neslihan_iu81@hotmail.com>
2024-11-22 21:59:51 -0500

    Adds form event

--- script.js
+++ script.js
@@ -15,3 +15,14 @@ const keyMessage = document.getElementById('keyMessage');
 keyInput.addEventListener('keyup', (event) => {
     keyMessage.textContent = `Last key pressed: ${event.key}`;
 });
+
+const sampleForm = document.getElementById('sampleForm');
+const formMessage = document.getElementById('formMessage');
+
+sampleForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const message = document.getElementById('message').value;
+    formMessage.textContent = `Form submitted! Name: ${firstName} ${lastName}, Message: ${message}`;
+});
